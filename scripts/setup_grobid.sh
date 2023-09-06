14fe8124abaae8871fad20fd125df6fdd2bc3f29
Adarsh <37295203+Adarsh0047@users.noreply.github.com>
2023-09-06 18:27:37 +0530

    Update setup_grobid.sh

--- scripts/setup_grobid.sh
+++ scripts/setup_grobid.sh
@@ -5,17 +5,17 @@ export DOC2JSON_HOME=$HOME/s2orc-doc2json
 
 # # Download Grobid
 cd $HOME
-wget https://github.com/kermitt2/grobid/archive/0.7.1.zip
-unzip 0.7.1.zip
-rm 0.7.1.zip
-cd $HOME/grobid-0.7.1
+wget https://github.com/kermitt2/grobid/archive/0.7.2.zip
+unzip 0.7.2.zip
+rm 0.7.2.zip
+cd $HOME/grobid-0.7.2
 ./gradlew clean install
 
 ## Grobid configurations
 # increase max.connections to slightly more than number of processes
 # decrease logging level
 # this isn't necessary but is nice to have if you are processing lots of files
-cp $DOC2JSON_HOME/doc2json/grobid2json/grobid/grobid.yaml $HOME/grobid-0.7.1/grobid-home/config/grobid.yaml
+cp $DOC2JSON_HOME/doc2json/grobid2json/grobid/grobid.yaml $HOME/grobid-0.7.2/grobid-home/config/grobid.yaml
 
 ## Start Grobid
 ./gradlew run
